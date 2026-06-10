8f7e712da8cef80c0b6dcb540f6213d28ee15dc6
wow1324332 <wow1324332@gmail.com>
2026-06-10 16:27:00 +0900

    Update DevicesModule.jsx

--- src/components/DevicesModule.jsx
+++ src/components/DevicesModule.jsx
@@ -785,7 +785,10 @@ export const DevicesDashboard = ({ user, onNavigate, onLogout, onQuit }) => {
         </div>
       </header>
 
-      <div className="flex flex-1 overflow-hidden relative bg-[#f0f2f5] bg-cover bg-center bg-no-repeat">
+      <div 
+        className="flex flex-1 overflow-hidden relative bg-[#f0f2f5] bg-cover bg-center bg-no-repeat"
+        style={{ backgroundImage: "url('/project-bg.jpg')" }}
+      >
         <aside className={`bg-white border-r border-gray-100 transition-all duration-300 ease-in-out flex flex-col z-10 overflow-hidden whitespace-nowrap ${sidebarOpen ? 'w-64 translate-x-0' : 'w-0 -translate-x-full'}`}>
           <div className="p-4 space-y-1 w-64">
             <div className="text-xs font-semibold text-gray-400 tracking-wider mb-4 px-3 mt-2">MENU</div>
